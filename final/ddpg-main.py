4b9eae8ea2fe12915cfe86f090d9a031e56c387b
hanskyy <hanskyy@gmail.com>
2021-04-16 07:04:18 -0500

    create 2 new files for ddpg

---
+++ final/ddpg-main.py
@@ -0,0 +1,75 @@
+#from ddpg-rl import DDPG
+import numpy as np
+import gym
+
+ON_TRAIN = True
+
+# set env as Fetch and set dim
+env = gym.make('FetchReach-v1')
+desiredgoal_shape = env.observation_space['desired_goal'].shape[0]    #desired_goal is 3
+achievedgoal_shape = env.observation_space['achieved_goal'].shape[0]   # 3
+observation_shape = env.observation_space['observation'].shape[0]      # 10
+a_dim = env.action_space.shape[0]                               # 4
+s_dim = observation_shape + achievedgoal_shape + desiredgoal_shape  # 16
+#s_dim = 13
+#a_dim = 4
+a_bound = env.action_space.high[1]
+
+max_action = env.action_space.high[0]
+min_action = env.action_space.low[0]
+
+# state = env.reset().values()
+# obs, curr_pos, goal_pos = state
+# exp = np.concatenate((obs, curr_pos, goal_pos))
+# exp.reshape((s_dim,))
+rl = DDPG(input_dims=s_dim, env=env, n_actions=a_dim)
+
+MAX_EPISODES = 5000
+MAX_EP_STEPS = 200
+
+def train():
+    # start training
+    for i in range(MAX_EPISODES):
+
+        state = env.reset().values()
+        obs, curr_pos, goal_pos = state
+        init_pos = curr_pos
+        ep_reward = 0
+
+        for j in range(MAX_EP_STEPS):
+            # env.render()
+            action = rl.choose_action(np.concatenate((obs, curr_pos, goal_pos)))
+
+            next_state, reward, done, info = env.step(action)
+            next_obs, next_curr_pos, goal_pos = next_state.values()
+
+            #send new experience to replay buffer
+            exp = np.concatenate((obs, curr_pos, goal_pos))
+            next_exp = np.concatenate((next_obs, next_curr_pos, goal_pos))
+            rl.store_transition(exp, action, reward, next_exp, done)
+
+
+            if rl.memory_full:
+                # start to learn once has fulfilled the memory
+                rl.learn()
+            # update the states
+            curr_pos = next_curr_pos
+            obs = next_obs
+            ep_reward += reward
+
+            if done or j == MAX_EP_STEPS-1:
+                print('Ep: %i | %s | ep_r: %.1f | step: %i' % (i, '---' if not done else 'done', ep_reward, j))
+                break
+    rl.save()
+
+
+# def eval():
+
+
+
+
+
+# if ON_TRAIN:
+#     train()
+# else:
+#     eval()
---
+++ final/ddpg-rl.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+import numpy as np
+
+#####################  hyper parameters  ####################
+
+LR_A = 0.001    # learning rate for actor
+LR_C = 0.001    # learning rate for critic
+GAMMA = 0.9     # reward discount
+TAU = 0.01      # soft replacement
+MEMORY_CAPACITY = 10000
+BATCH_SIZE = 32
+
+class DDPG(object):
+    def __init__(self, input_dims, env=None, n_actions=4):
+        self.n_actions = n_actions
+        self.max_action = env.action_space.high[0]
+        self.min_action = env.action_space.low[0]
+        self.mem_size = MEMORY_CAPACITY
+        self.mem_cntr = 0
+        self.input_dims = input_dims
+        self.state_memory = np.zeros((self.mem_size, input_dims))
+        self.new_state_memory = np.zeros((self.mem_size, input_dims))
+        self.action_memory = np.zeros((self.mem_size, self.n_actions))
+        self.reward_memory = np.zeros(self.mem_size)
+        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+        self.memory_full = False
+        self.sess = tf.Session()
+        self.a_replace_counter, self.c_replace_counter = 0, 0
+
+        self.S = tf.placeholder(tf.float32, [None, input_dims], 's')
+        self.S_ = tf.placeholder(tf.float32, [None, input_dims], 's_')
+        self.R = tf.placeholder(tf.float32, [None, 1], 'r')
+
+        with tf.variable_scope('Actor'):
+            self.a = self._build_a(self.S, scope='eval', trainable=True)
+            a_ = self._build_a(self.S_, scope='target', trainable=False)
+        with tf.variable_scope('Critic'):
+            # assign self.a = a in memory when calculating q for td_error,
+            # otherwise the self.a is from Actor when updating Actor
+            q = self._build_c(self.S, self.a, scope='eval', trainable=True)
+            q_ = self._build_c(self.S_, a_, scope='target', trainable=False)
+
+        # networks parameters
+        self.ae_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval')
+        self.at_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target')
+        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
+        self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
+
+        # target net replacement
+        self.soft_replace = [[tf.assign(ta, (1 - TAU) * ta + TAU * ea), tf.assign(tc, (1 - TAU) * tc + TAU * ec)]
+                             for ta, ea, tc, ec in zip(self.at_params, self.ae_params, self.ct_params, self.ce_params)]
+
+        q_target = self.R + GAMMA * q_
+        td_error = tf.losses.mean_squared_error(labels=q_target, predictions=q)
+        self.ctrain = tf.train.AdamOptimizer(LR_C).minimize(td_error, var_list=self.ce_params)
+
+        a_loss = - tf.reduce_mean(q)  # maximize the q
+        self.atrain = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=self.ae_params)
+
+        self.sess.run(tf.global_variables_initializer())
+
+    def store_transition(self, state, action, reward, new_state, done):
+        index = self.mem_cntr % self.mem_size
+        self.state_memory[index] = state
+        self.new_state_memory[index] = new_state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+        self.mem_cntr +=1
+
+    def sample_buffer(self, batch_size):
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, batch_size, replace=False)
+        states = self.state_memory[batch]
+        _states = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+        return states, actions, rewards, _states, dones
+
+
+
+
+    """test code"""
+    def _build_a(self, s, scope, trainable):
+        with tf.variable_scope(scope):
+            net = tf.layers.dense(s, 300, activation=tf.nn.relu, name='l1', trainable=trainable)
+            a = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, name='a', trainable=trainable)
+            return tf.multiply(a, self.a_bound, name='scaled_a')
+
+    def _build_c(self, s, a, scope, trainable):
+        with tf.variable_scope(scope):
+            n_l1 = 300
+            w1_s = tf.get_variable('w1_s', [self.s_dim, n_l1], trainable=trainable)
+            w1_a = tf.get_variable('w1_a', [self.a_dim, n_l1], trainable=trainable)
+            b1 = tf.get_variable('b1', [1, n_l1], trainable=trainable)
+            net = tf.nn.relu(tf.matmul(s, w1_s) + tf.matmul(a, w1_a) + b1)
+            return tf.layers.dense(net, 1, trainable=trainable)  # Q(s,a)
