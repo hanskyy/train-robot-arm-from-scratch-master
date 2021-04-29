8b655614466cc99de73fd6b92356a4594ba119f2
hanskyy <hanskyy@gmail.com>
2021-04-29 18:29:53 -0500

    add a ddpghopper implementation

--- final/ddpghopper.py
+++ final/ddpghopper.py
@@ -1,31 +1,29 @@
-from ddpgrl import DDPG
+from ddpgrlhopper import DDPG
 import numpy as np
 import gym
 import subprocess
 import matplotlib.pyplot as plt
 from PIL import Image
 
-ON_TRAIN = True
+ON_TRAIN = False
 
 
 # set env as Fetch and set dim
-env = gym.make('Hopper-v2')
+env = gym.make('Swimmer-v2')
 
-print(env.action_space.high[0])
-print(env.action_space)		#3
-print(env.observation_space)	#11
+# print(env.action_space.high[0])
+# print(env.action_space)		#3
+# print(env.observation_space)	#11
 
-desiredgoal_shape = env.observation_space['desired_goal'].shape[0]    #desired_goal is 3
-achievedgoal_shape = env.observation_space['achieved_goal'].shape[0]   # 3
-observation_shape = env.observation_space['observation'].shape[0]      # 10
-a_dim = env.action_space.shape[0]                               # 4
-s_dim = observation_shape + achievedgoal_shape + desiredgoal_shape  # 16
+
+a_dim = env.action_space.shape[0]                               # 3
+s_dim = env.observation_space.shape[0]  # 11
 #s_dim = 13
 #a_dim = 4
-a_bound = env.action_space.high[1]
+a_bound = env.action_space.high[0]
 
-max_action = env.action_space.high[0]
-min_action = env.action_space.low[0]
+# max_action = env.action_space.high[0]
+# min_action = env.action_space.low[0]
 
 # state = env.reset().values()
 # obs, curr_pos, goal_pos = state
@@ -37,37 +35,33 @@ min_action = env.action_space.low[0]
 # next_curr_pos_log = []
 rl = DDPG(input_dims=s_dim, env=env, n_actions=a_dim)
 
-MAX_EPISODES = 20000
-MAX_EP_STEPS = 200
+MAX_EPISODES = 1000
+MAX_EP_STEPS = 500
 
 def train():
     # start training
     for i in range(MAX_EPISODES):
 
-        state = env.reset().values()
-        obs, curr_pos, goal_pos = state
-        init_pos = curr_pos
+        state = env.reset()
+        # state = env.reset().values()
         ep_reward = 0
 
         for j in range(MAX_EP_STEPS):
             # env.render()
-            action = rl.choose_action(np.concatenate((obs, curr_pos, goal_pos)))
+            action = rl.choose_action(state)
 
-            next_state, reward, done, info = env.step(action)
-            next_obs, next_curr_pos, goal_pos = next_state.values()
+            next_state, reward, done, _ = env.step(action)
+            # next_obs, next_curr_pos, goal_pos = next_state.values()
 
             #send new experience to replay buffer
-            exp = np.concatenate((obs, curr_pos, goal_pos))
-            next_exp = np.concatenate((next_obs, next_curr_pos, goal_pos))
-            rl.store_transition(exp, action, reward, next_exp, done)
+            rl.store_transition(state, action, reward, next_state, done)
 
             # start to learn once has fulfilled the memory
             if rl.memory_full:
                 rl.learn()
 
             # update the states
-            curr_pos = next_curr_pos
-            obs = next_obs
+            state = next_state
             ep_reward += reward
 
             if done or j == MAX_EP_STEPS-1:
@@ -78,16 +72,16 @@ def train():
 
 def eval():
     rl.restore()
-    state = env.reset().values()
-    obs, curr_pos, goal_pos = state
+    state = env.reset()
+    # state = env.reset().values()
     subprocess.call(['rm', '-rf', 'frames'])
     subprocess.call(['mkdir', '-p', 'frames'])
     time_step_counter = 0
     while True:
         #env.render()
-        action = rl.choose_action(np.concatenate((obs, curr_pos, goal_pos)))
-        next_state, reward, done, info = env.step(action)
-        next_obs, next_curr_pos, goal_pos = next_state.values()
+        action = rl.choose_action(state)
+        next_state, reward, done, _ = env.step(action)
+        # next_obs, next_curr_pos, goal_pos = next_state.values()
 
         #img = env.render(height=480, width=480)
         image_data = env.render(mode="rgb_array")
@@ -97,9 +91,8 @@ def eval():
         img = Image.fromarray(image_data, 'RGB')
         img.save("frames/frame-%.10d.png" % time_step_counter)
         time_step_counter += 1
-        curr_pos = next_curr_pos
-        obs = next_obs
-        if done:
+        state = next_state
+        if done or time_step_counter > 100:
             break
     subprocess.call([
         'ffmpeg', '-framerate', '50', '-y', '-i', 'frames/frame-%010d.png', '-r', '30', '-pix_fmt', 'yuv420p',
